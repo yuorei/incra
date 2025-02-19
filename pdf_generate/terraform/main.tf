260f15d8184b3b91c21c232b0ca1e5f33c8244b0
yuorei <108039575+yuorei@users.noreply.github.com>
2025-02-19 10:55:19 +0900

    fix: enable AWS credentials configuration in Terraform workflow and remove unused variables

--- pdf_generate/terraform/main.tf
+++ pdf_generate/terraform/main.tf
@@ -1,21 +1,9 @@
-variable "access_key" {
-  description = "AWS access key"
-  type        = string
-}
-
-variable "secret_key" {
-  description = "AWS secret key"
-  type        = string
-}
-
 variable "region" {
   description = "AWS region"
   type        = string
 }
 
 provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
   region     = var.region
 }
 
